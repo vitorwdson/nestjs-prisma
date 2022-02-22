fb5d41cbd55b612ee2e2015f5fd04f7255bce844
Vitor Wdson <vitor.wdson2@gmail.com>
2022-02-21 22:55:24 -0400

    Loading the server port from Env Variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+let port = parseInt(process.env.PORT);
+if (isNaN(port)) {
+  port = 4000;
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(port);
 }
 bootstrap();
