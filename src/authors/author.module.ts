a461fb1b4c9f724c04525dfb5c8329872b45a2ef
Vitor Wdson <vitor.wdson2@gmail.com>
2022-02-22 21:37:29 -0400

    Created the Author Service

--- src/authors/author.module.ts
+++ src/authors/author.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 import { AuthorsResolver } from './author.resolver';
 import { AuthorsService } from './author.service';
 
 @Module({
-  providers: [AuthorsResolver, AuthorsService],
+  providers: [PrismaService, AuthorsService, AuthorsResolver],
 })
 export class AuthorsModule {}
--- src/authors/author.service.ts
+++ src/authors/author.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { Author as AuthorModel, Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
-export class AuthorsService {}
+export class AuthorsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(args?: Prisma.AuthorFindManyArgs): Promise<AuthorModel[]> {
+    return this.prisma.author.findMany(args);
+  }
+
+  async findOne(args?: Prisma.AuthorFindFirstArgs): Promise<AuthorModel> {
+    return this.prisma.author.findFirst(args);
+  }
+
+  async create(data: AuthorModel): Promise<AuthorModel> {
+    return this.prisma.author.create({
+      data,
+    });
+  }
+
+  async update(
+    authorId: number,
+    data: Prisma.AuthorUpdateInput,
+  ): Promise<AuthorModel> {
+    return this.prisma.author.update({
+      where: {
+        id: authorId,
+      },
+      data,
+    });
+  }
+}
